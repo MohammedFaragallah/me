15e9406ebe51ccc2192acd3bf379daa8c3be04c6
Mohammed Faragallah <o0frego0o@hotmail.com>
2019-12-03 20:46:51 +0200

    Updates

---
+++ precache-manifest.ebf2b224f767ad06d19ceb1f12077b5c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a6ab084230bfce37cdebc3b785c3ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a6270cfb22f74eb8a99",
+    "url": "/static/js/2.61d4c4f5.chunk.js"
+  },
+  {
+    "revision": "711f424df4ba75a20ef7",
+    "url": "/static/js/3.8eda9fcd.chunk.js"
+  },
+  {
+    "revision": "6ba3b9aa7d92a2763745",
+    "url": "/static/js/4.34d3b3a8.chunk.js"
+  },
+  {
+    "revision": "942263d649a3ba1fa658",
+    "url": "/static/js/5.19369da7.chunk.js"
+  },
+  {
+    "revision": "cbb0c365ea5fa7ecb24b",
+    "url": "/static/js/6.070ddb0f.chunk.js"
+  },
+  {
+    "revision": "5ab05654d68132428660",
+    "url": "/static/js/main.7b2908a0.chunk.js"
+  },
+  {
+    "revision": "d0e1d3d440f18cd747ec",
+    "url": "/static/js/runtime-main.254b3300.js"
+  },
+  {
+    "revision": "a94060a54715942992b60c72a8fb7fde",
+    "url": "/static/media/boom.a94060a5.mp3"
+  },
+  {
+    "revision": "1d4356c2e32f881ceed2a976ec4eca64",
+    "url": "/static/media/clap.1d4356c2.mp3"
+  },
+  {
+    "revision": "762ffd4c92ad51047ef89f3fa4e56edb",
+    "url": "/static/media/hihat.762ffd4c.mp3"
+  },
+  {
+    "revision": "3a495231ab0d1307ed9d950eff7ea008",
+    "url": "/static/media/kick.3a495231.mp3"
+  },
+  {
+    "revision": "1304bd0fac5d65a1f41e9ecc2531beed",
+    "url": "/static/media/openhat.1304bd0f.mp3"
+  },
+  {
+    "revision": "3aa8471ffbb8f831993b42b144f65e4a",
+    "url": "/static/media/ride.3aa8471f.mp3"
+  },
+  {
+    "revision": "b1f4e0848d3c4268324f376246328e6f",
+    "url": "/static/media/snare.b1f4e084.mp3"
+  },
+  {
+    "revision": "dbd89267a1ff54e67ed73f2a90604906",
+    "url": "/static/media/tink.dbd89267.mp3"
+  },
+  {
+    "revision": "db3796bcacb5655e8730082a054384ab",
+    "url": "/static/media/tom.db3796bc.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.6c294acc1cd1ae14ea7b69cb58a43ff3.js"
+  "/precache-manifest.ebf2b224f767ad06d19ceb1f12077b5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
